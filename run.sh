c870e02ac7fdd5e1f36083d37d23b30c84cbf229
Alexander Kras'ko <0m3r.mail@gmail.com>
2016-01-05 22:42:12 +0200

    satis build satis.json .

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo 'git pull'
 git pull
+echo 'rm -rf include/*'
 rm -rf include/*	
+echo 'satis build satis.json .'
 satis build satis.json .
+echo 'git add . '
 git add . 
+echo 'git commit -m "satis build satis.json ."'
 git commit -m "satis build satis.json ."
+echo 'git push'
 git push
